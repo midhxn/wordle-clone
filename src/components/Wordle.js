42f8be1ef1815569cf2d74f89d7a8b0e24a9bd56
midhxn <midhunn311@gmail.com>
2022-07-04 23:58:54 +0530

    commit

---
+++ src/components/Wordle.js
@@ -0,0 +1,21 @@
+import React,{useEffect} from 'react'
+import useWordle from '../hooks/useWordle.js'
+
+export default function Wordle({solution}) {
+
+  const {currentGuess,handleKeyUp} = useWordle(solution)
+
+  useEffect(() => {
+    
+    window.addEventListener('keyup',handleKeyUp)
+
+    return () => window.removeEventListener('keyup',handleKeyUp)
+  }, [handleKeyUp])
+    
+  return (
+    <div>
+        <div>current:{currentGuess}</div>
+    </div>
+    
+  )
+}
