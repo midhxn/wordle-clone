15b298e298a929593f97ec38dcbcfb862347e5dd
midhxn <midhunn311@gmail.com>
2022-07-04 23:59:02 +0530

    commit

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,14 +1,15 @@
-import React, {useEffect,useState} from 'react';
+import React,{useState} from 'react'
 
-const useWordle = (soliution) => {
-    const [turn,setTurn] = useState(0);
-    const [currentGuess,setCurrentGuess] = useState('');
-    const [guesses,setGuesses]=useState([]);
-    const [history,setHistory]=useState([]);
-    const [isCorrect,setIsCorrect]=useState(false);
+const useWordle = (solution) => {
+    const [turn,setTurn] = useState(0)
+    const [currentGuess,setCurrentGuess] = useState('')
+    const [guesses,setGuesses]=useState([])
+    const [history,setHistory]=useState([])
+    const [isCorrect,setIsCorrect]=useState(false)
 
 
     const formatGuess = () =>{
+        
 
     }
     
@@ -16,10 +17,41 @@ const useWordle = (soliution) => {
         
     }
 
-    const handleKeyUp = () =>{
-        
-    }
 
+    //handle keypress
+    const handleKeyUp = ({key}) =>{
+
+
+        console.log('key pressed ');
+
+        if(key==='Enter'){
+            if(turn < 5){
+                console.log("No Way")
+                return
+            }
+            if(history.includes(currentGuess)){
+                console.log("No Way2")
+                return
+            }       
+            
+            if(currentGuess.length>5){
+                console.log("No Way3")
+                return;
+            }
+        }
+        if(key==='Backspace'){
+            setCurrentGuess((prev)=>{
+                return prev.slice(0,-1)
+            })
+            return
+        }
+
+        if(/^[A-Za-z]$/.test(key)){
+            if(currentGuess.length < 5){
+                setCurrentGuess(prev =>  prev + key)
+            }
+        }
+    }
     return(turn,currentGuess,guesses,isCorrect,handleKeyUp)
 }
 
