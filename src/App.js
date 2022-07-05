8c0c8c890aea4859508179b626d703278d2a8e4a
midhxn <midhunn311@gmail.com>
2022-07-05 19:14:20 +0530

    updated💯⚠️

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React,{useEffect,useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import Wordle from './components/Wordle.js'
+import { useEffect, useState } from 'react'
+import Wordle from './components/Wordle'
 
 function App() {
-
-  const[solution,setSolution] = useState(null);
-
+  const [solution, setSolution] = useState(null)
+  
   useEffect(() => {
     fetch('http://localhost:3001/solutions')
-    .then(res => res.json())
-    .then(json=>{
-      const randSolution = json[Math.floor(Math.random()*json.length)];
-      setSolution(randSolution.word);
-    })
-    },[setSolution])
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
+
+  
   return (
     <div className="App">
-      <h1>Wordle Clone</h1>
-      {solution && <Wordle solution={solution}/> }
+      <h1>Wordle (Lingo)</h1>
+      {solution && <Wordle solution={solution} />}
     </div>
   )
 }
 
 export default App
+
