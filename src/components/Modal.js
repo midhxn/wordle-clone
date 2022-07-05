2aa154fd63585e20cbb2caf7afcdd651816c2a66
midhxn <midhunn311@gmail.com>
2022-07-05 19:11:26 +0530

    update in modal file👣

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Modal({ isCorrect, solution, turn }) {
+  return (
+    <div className="modal">
+      {isCorrect && (
+        <div>
+          <h1>Hurrayyy! You Won</h1>
+          <p className="solution">{solution}</p>
+          <p>In just {turn} guesses you found the solution</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Try again warrior</h1>
+          <p className="solution">Answer: {solution}</p>
+          <p>Better luck next time </p>
+        </div>
+      )}
+    </div>
+  )
+}
